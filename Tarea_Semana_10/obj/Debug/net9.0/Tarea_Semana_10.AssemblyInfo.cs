e4a03e2134359ce6e24c5a22c808a5d7600d5888
Alexis uea <ae.merinom@uea.edu.ec>
2025-02-18 11:58:20 -0500

    -

--- Tarea_Semana_10/obj/Debug/net9.0/Tarea_Semana_10.AssemblyInfo.cs
+++ Tarea_Semana_10/obj/Debug/net9.0/Tarea_Semana_10.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea_Semana_10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72bf9d2eb7989ccce212669c2b3bd12db2b582a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de6eeade1ae2ad1861c207b87bf7f41b87da021c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea_Semana_10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea_Semana_10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
